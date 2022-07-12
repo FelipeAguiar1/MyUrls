7d84d3484a97152332c6db138aaa0b61d8ae7a8b
FelipeAguiar1 <felipeedu1516@gmail.com>
2022-07-12 20:34:32 -0300

    main

--- api/index.js
+++ api/index.js
@@ -3,25 +3,39 @@ const URL = require('url');
 const fs = require('fs');
 const data = require('./urls.json');
 const path = require('path');
-const { ok } = require('assert');
+
+function writeFile(cb) {
+    fs.writeFile(
+        path.join(__dirname, "urls.json"), 
+        JSON.stringify(data, null, 2),
+        (err) => {
+            if (err) throw err;
+
+            cb(JSON.stringify({message: "ok"}))
+        }
+    )
+}
 
 http.createServer((req, res) => {
     const { name, url, del } = URL.parse(req.url, true).query;
+
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*'
+    })
 
     if (!name || !url) {
         return res.end(JSON.stringify(data));
     } 
     if (del) {
-        data.urls = data.urls.filter(item => item.url !== url);
-        fs.writeFile(
-            path.join(__dirname, "urls.json"), 
-            JSON.stringify(data, null, 2),
-            (err) => {
-                if (err) throw err;
-
-                res.end(JSON.stringify({message: ok}))
-            }
-            )
+        data.urls = data.urls.filter(item => String(item.url) !== String(url));
+        writeFile((message) => {
+            return res.end(message);
+        })
     }
-    return res.end('create');
+
+    data.urls.push({name, url});
+
+    return writeFile((message) => {
+        res.end(message);
+    })
 }).listen(3000, () => console.log('API is reunning...'));
